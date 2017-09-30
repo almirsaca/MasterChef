f75152f2e91b46d45e48d2188485642de5129db7
wagner davanco <wagnerdavanco@gmail.com>
2017-09-30 13:30:33 -0300

    add interfaces no projeto api

--- MasterChef.Api/Startup.cs
+++ MasterChef.Api/Startup.cs
@@ -8,6 +8,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MasterChef.Domain;
+using MasterChef.Repository;
+using MasterChef.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using MasterChef.Application;
+using MasterChef.Application.Interfaces;
 
 namespace MasterChef.Api
 {
@@ -23,6 +29,12 @@ namespace MasterChef.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Add(ServiceDescriptor.Scoped(typeof(DbContext), typeof(MasterChefContext)));
+            services.Add(ServiceDescriptor.Scoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>)));
+            services.Add(ServiceDescriptor.Scoped(typeof(IUnitOfWork), typeof(UnitOfWork)));
+            services.Add(ServiceDescriptor.Scoped(typeof(IReceitaRepository), typeof(ReceitaRepository)));
+            services.Add(ServiceDescriptor.Scoped(typeof(IReceitaService), typeof(ReceitaService)));
+
             services.AddMvc();
         }
 
